659bc1b0ccb09ebb2b8e46d928b87e262c7de3fe
jGorny <iceziro@wp.pl>
2019-10-13 18:55:07 +0200

    added name,surname,email,bday also edited toString,hashC,equals

--- src/main/java/pl/edu/pjwstk/login/model/User.java
+++ src/main/java/pl/edu/pjwstk/login/model/User.java
@@ -7,6 +7,10 @@ import java.util.Objects;
 public class User implements Serializable {
     private String pass;
     private String login;
+    private String surname;
+    private String name;
+    private String bday;
+    private String email;
 
     public String getPass() {
         return pass;
@@ -24,12 +28,8 @@ public class User implements Serializable {
         this.login = login;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "pass='" + pass + '\'' +
-                ", login='" + login + '\'' +
-                '}';
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     @Override
@@ -38,11 +38,55 @@ public class User implements Serializable {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
         return Objects.equals(pass, user.pass) &&
-                Objects.equals(login, user.login);
+                Objects.equals(login, user.login) &&
+                Objects.equals(surname, user.surname) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(bday, user.bday) &&
+                Objects.equals(email, user.email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "pass='" + pass + '\'' +
+                ", login='" + login + '\'' +
+                ", surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", bday='" + bday + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(pass, login);
+        return Objects.hash(pass, login, surname, name, bday, email);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBday(String bday) {
+        this.bday = bday;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBday() {
+        return bday;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
