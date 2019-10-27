723954dcd944a6a92909d2e36775284aa2418ed1
jGorny <iceziro@wp.pl>
2019-10-27 17:19:03 +0100

    last commit for 27.10.19

--- src/main/java/pl/edu/pjwstk/login/Login.java
+++ src/main/java/pl/edu/pjwstk/login/Login.java
@@ -4,13 +4,10 @@ package pl.edu.pjwstk.login;
 import pl.edu.pjwstk.login.model.User;
 
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.IOException;
 import java.io.Serializable;
 
 
