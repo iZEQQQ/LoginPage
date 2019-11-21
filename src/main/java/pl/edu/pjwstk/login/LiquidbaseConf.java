093bbe20183d7cb92a0484e247bcf431b74c877a
jGorny <iceziro@wp.pl>
2019-11-21 17:50:24 +0100

    uncommenting commit

--- src/main/java/pl/edu/pjwstk/login/LiquidbaseConf.java
+++ src/main/java/pl/edu/pjwstk/login/LiquidbaseConf.java
@@ -1,42 +1,42 @@
-//package pl.edu.pjwstk.login;
-//
-//import com.zaxxer.hikari.HikariConfig;
-//import com.zaxxer.hikari.HikariDataSource;
-//import liquibase.integration.cdi.CDILiquibaseConfig;
-//import liquibase.integration.cdi.annotations.LiquibaseType;
-//import liquibase.resource.ClassLoaderResourceAccessor;
-//import liquibase.resource.ResourceAccessor;
-//
-//import javax.enterprise.context.ApplicationScoped;
-//import javax.enterprise.inject.Produces;
-//import javax.sql.DataSource;
-//
-//@ApplicationScoped
-//public class LiquidbaseConf {
-//    @Produces
-//    @LiquibaseType
-//    public CDILiquibaseConfig createConfig() {
-//        CDILiquibaseConfig config = new CDILiquibaseConfig();
-//        config.setChangeLog("db.changelog/db.changelog.yaml");
-//        return config;
-//    }
-//
-//    @Produces
-//    @LiquibaseType
-//    public DataSource createDataSource() {
-//        var config = new HikariConfig();
-//        config.setDriverClassName("org.postgresql.Driver");
-//        config.setJdbcUrl("jdbc:postgresql://localhost:5432/jazapp");
-//        config.setUsername("jazapp");
-//        config.setPassword("jazapp!98");
-//
-//        return new HikariDataSource(config);
-//    }
-//
-//    @Produces
-//    @LiquibaseType
-//    public ResourceAccessor create() {
-//        return new ClassLoaderResourceAccessor(getClass().getClassLoader());
-//    }
-//
-//}
+package pl.edu.pjwstk.login;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import liquibase.integration.cdi.CDILiquibaseConfig;
+import liquibase.integration.cdi.annotations.LiquibaseType;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.ResourceAccessor;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.sql.DataSource;
+
+@ApplicationScoped
+public class LiquidbaseConf {
+    @Produces
+    @LiquibaseType
+    public CDILiquibaseConfig createConfig() {
+        CDILiquibaseConfig config = new CDILiquibaseConfig();
+        config.setChangeLog("db.changelog/db.changelog.yaml");
+        return config;
+    }
+
+    @Produces
+    @LiquibaseType
+    public DataSource createDataSource() {
+        var config = new HikariConfig();
+        config.setDriverClassName("org.postgresql.Driver");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/loginpage");
+        config.setUsername("loginpage");
+        config.setPassword("loginpage");
+
+        return new HikariDataSource(config);
+    }
+
+    @Produces
+    @LiquibaseType
+    public ResourceAccessor create() {
+        return new ClassLoaderResourceAccessor(getClass().getClassLoader());
+    }
+
+}
