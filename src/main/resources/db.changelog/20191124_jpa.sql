66b6be948b0202975a58809bfea813febed25af6
jGorny <iceziro@wp.pl>
2019-11-28 19:10:10 +0100

    added something to sql

--- src/main/resources/db.changelog/20191124_jpa.sql
+++ src/main/resources/db.changelog/20191124_jpa.sql
@@ -1,25 +1,36 @@
 CREATE TABLE auction
 (
-    id   BIGINT  NOT NULL,
-    name VARCHAR NOT NULL,
+    id            BIGINT        NOT NULL,
+    title         VARCHAR       NOT NULL,
+    description   VARCHAR       NOT NULL,
+    price         NUMERIC(9, 2) NOT NULL,
+    parameters_id BIGINT        NOT NULL,
+    photos_id     BIGINT        NOT NULL,
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (parameters_id) REFERENCES parameters (id),
+    FOREIGN KEY (photos_id) REFERENCES photos (id)
 );
 
 
-CREATE TABLE parameters(
-
+CREATE TABLE parameters
+(
+    id BIGINT NOT NULL
 );
 
 
-CREATE TABLE photos(
+CREATE TABLE photos
+(
+    id BIGINT NOT NULL,
+    photo_link VARCHAR NOT NULL
+);
+
+CREATE TABLE branches
+(
 
 );
 
-CREATE TABLE branches(
-
-);
-
-CREATE TABLE categories(
+CREATE TABLE categories
+(
 
 );
