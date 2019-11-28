b6c116270c3ed9959feec89758ced9825919fcc8
jGorny <iceziro@wp.pl>
2019-11-28 20:07:50 +0100

    added something to sql

--- src/main/resources/db.changelog/20191124_jpa.sql
+++ src/main/resources/db.changelog/20191124_jpa.sql
@@ -4,31 +4,37 @@ CREATE TABLE auction
     title         VARCHAR       NOT NULL,
     description   VARCHAR       NOT NULL,
     price         NUMERIC(9, 2) NOT NULL,
-    parameters_id BIGINT        NOT NULL,
-    photos_id     BIGINT        NOT NULL,
+
+    -- moje aukcje
 
     PRIMARY KEY (id),
-    FOREIGN KEY (parameters_id) REFERENCES parameters (id),
-    FOREIGN KEY (photos_id) REFERENCES photos (id)
+    FOREIGN KEY (parameters_id) REFERENCES parameters (id)--,
+ --   FOREIGN KEY (photos_id) REFERENCES photos (id)
 );
 
 
-CREATE TABLE parameters
+CREATE TABLE parameter
 (
     id BIGINT NOT NULL
+-- nazwa
 );
 
+-- brakuje tabeli laczacej parametr z wartoscia w aukcji
 
-CREATE TABLE photos
+
+CREATE TABLE photo
 (
-    id         BIGINT  NOT NULL,
-    photo_link VARCHAR NOT NULL
+    -- brakuje id
+  link VARCHAR NOT NULL,
+    auction_id BIGINT NOT NULL,
+    PRIMARY KEY(auction_id),-- blad!!!
+        FOREIGN KEY (auction_id) REFERENCES auction (id)
 );
 
-CREATE TABLE branches
+CREATE TABLE branches -- liczba pojedyncza
 (
     id          BIGINT  NOT NULL,
-    branch      VARCHAR NOT NULL,
+    branch      VARCHAR NOT NULL, -- nazwa!
     category_id BIGINT  NOT NULL,
     FOREIGN KEY (category_id) REFERENCES categories (id)
 );
