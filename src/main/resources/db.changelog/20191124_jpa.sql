85fcaf92ce1573709cb1e18e09d6e8c2f2b53ead
jGorny <iceziro@wp.pl>
2019-12-05 18:23:51 +0100

    moved primary key declaration

--- src/main/resources/db.changelog/20191124_jpa.sql
+++ src/main/resources/db.changelog/20191124_jpa.sql
@@ -1,52 +1,59 @@
 CREATE TABLE parameter
 (
-    id   SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL
+    id   SERIAL,
+    name VARCHAR NOT NULL,
+
+    PRIMARY KEY (id)
 
 );
 
 CREATE TABLE branch
 (
-    id   SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL
+    id   SERIAL,
+    name VARCHAR NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE category
 (
-    id        SERIAL PRIMARY KEY,
+    id        SERIAL,
     name      VARCHAR NOT NULL,
     branch_id BIGINT  not null,
+    PRIMARY KEY (id),
     FOREIGN KEY (branch_id) REFERENCES branch (id)
 
 );
 
 CREATE TABLE auction
 (
-    id          SERIAL PRIMARY KEY,
+    id          SERIAL,
     title       VARCHAR       NOT NULL,
     description VARCHAR       NOT NULL,
     price       NUMERIC(9, 2) NOT NULL,
     category_id BIGINT        not null,
-    user_id     VARCHAR       not null,
+    owner_id    VARCHAR       not null,
+    PRIMARY KEY (id),
     FOREIGN KEY (category_id) REFERENCES category (id),
-    FOREIGN KEY (user_id) REFERENCES users (login)
+    FOREIGN KEY (owner_id) REFERENCES users (login)
 );
 
 CREATE TABLE photo
 (
-    id         SERIAL PRIMARY KEY,
+    id         SERIAL,
     link       VARCHAR NOT NULL,
     auction_id BIGINT  NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (auction_id) REFERENCES auction (id)
 );
 
 CREATE TABLE auction_parameter
 (
-    id           SERIAL PRIMARY KEY,
+    id           SERIAL,
     number_value BIGINT,
     text_value   TEXT,
     auction_id   int not null,
     parameter_id int not null,
+    PRIMARY KEY (id),
     FOREIGN KEY (auction_id) REFERENCES auction (id),
     FOREIGN KEY (parameter_id) REFERENCES parameter (id)
 
