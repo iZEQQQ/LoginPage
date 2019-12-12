d03ec525dc4a478ded30a088256e8c811abd2a83
jGorny <iceziro@wp.pl>
2019-12-12 17:57:17 +0100

    added AuctionList

--- src/main/java/pl/edu/pjwstk/login/auction/view/AuctionList.java
+++ src/main/java/pl/edu/pjwstk/login/auction/view/AuctionList.java
@@ -1,4 +1,34 @@
 package pl.edu.pjwstk.login.auction.view;
 
-public class AuctionList {
+
+import pl.edu.pjwstk.login.auction.model.Auction;
+import pl.edu.pjwstk.login.auction.service.AuctionService;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class AuctionList implements Serializable {
+
+    @Inject
+    private AuctionService service;
+    private List<Auction> auctionList;
+
+    public List<Auction> getAuctionList() {
+        if (auctionList == null) {
+            auctionList = service.findAllAuctions();
+        }
+        return auctionList;
+    }
+
+    public String delete(Auction auction) {
+        service.remove(auction);
+        auctionList.remove(auction);
+        return null;
+    }
+
 }
