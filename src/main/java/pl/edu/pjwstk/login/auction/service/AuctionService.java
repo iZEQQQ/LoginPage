1c1809da4fad9aa1b2b69af39268ce09488fe33e
jGorny <iceziro@wp.pl>
2019-12-12 17:47:28 +0100

    added AuctionService

---
+++ src/main/java/pl/edu/pjwstk/login/auction/service/AuctionService.java
@@ -0,0 +1,33 @@
+package pl.edu.pjwstk.login.auction.service;
+
+
+import pl.edu.pjwstk.login.auction.model.Auction;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
+public class AuctionService {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Auction> findAllAuctions(){
+        return em.createQuery("SELECT a FROM Auction a", Auction.class).getResultList();
+    }
+
+    @Transactional
+    public void remove(Auction auction){
+        em.remove(em.merge(auction));
+    }
+
+    @Transactional
+    public void save(Auction auction){
+        em.persist(auction);
+    }
+
+
+}
