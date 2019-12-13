4155fd74675ffedb171183e182862b912746cfd5
jGorny <iceziro@wp.pl>
2019-12-13 14:14:20 +0100

    extended auction list and auction edit

--- src/main/resources/db.changelog/20191201_insert.sql
+++ src/main/resources/db.changelog/20191201_insert.sql
@@ -5,3 +5,9 @@ VALUES ('meat'),
 insert into category (name, branch_id)
 VALUES ('sausage', 1),
        ('onion', 2);
+
+
+insert into users (login,pass,name,surname,email,bday)
+VALUES ('Krzysio','Hello123','Krzysiek','Kowalski','hehe@hehe.pl',14/05/1999);
+
+
